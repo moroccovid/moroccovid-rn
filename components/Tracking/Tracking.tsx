7f0065dee32fe46f1ccb90b8fc3c0be1557fe792
MouadKH9 <mouad.khchich@gmail.com>
2020-04-13 23:04:24 +0100

    Progress on the map

--- components/Tracking/Tracking.tsx
+++ components/Tracking/Tracking.tsx
@@ -18,14 +18,18 @@ import Loading from '../utils/Loading/Loading';
 import Header from '../utils/Header/Header';
 import Geolocation from '@react-native-community/geolocation';
 import RNAndroidLocationEnabler from 'react-native-android-location-enabler';
-import {ListItem, Button} from 'react-native-elements';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import colors from '../../theme/colors';
+import MapView, {Marker, Polygon} from 'react-native-maps';
 
 export default class Tracking extends Component<{
   navigation: NavigationScreenProp<NavigationState, NavigationParams>;
 }> {
   state: any = {loading: false, staretd: false, error: false, points: []};
+
+  async componentDidMount() {
+    await this.getLocation();
+  }
 
   async checkPermission() {
     let permitted = await PermissionsAndroid.check(
@@ -53,15 +57,17 @@ export default class Tracking extends Component<{
         "Veuillez nous donner la permission d'obtenir votre position",
       );
 
-    console.log('Getting location');
-    this.setState({started: true});
+    console.log('Getting initial location');
     try {
-      let watchID = Geolocation.watchPosition(
+      Geolocation.getCurrentPosition(
         (info: any) => {
           console.log('Tracking -> getLocation -> info', info);
+
           let {points} = this.state;
-          points.push(info.coords);
+          points.push({...info.coords, timestamp: info.timestamp});
+          console.table(points);
           this.setState({
+            location: info.coords,
             points,
           });
         },
@@ -80,6 +86,47 @@ export default class Tracking extends Component<{
         },
         {enableHighAccuracy: true},
       );
+    } catch (error) {
+      console.error('error getting location: ', error);
+      this.showError('Un erreur est survenue, veuillez réessayer plus tard.');
+    }
+  }
+
+  async watchLocation(retry: boolean = true) {
+    let granted = await this.checkPermission();
+    if (!granted)
+      return this.showError(
+        "Veuillez nous donner la permission d'obtenir votre position",
+      );
+
+    console.log('Getting location');
+    this.setState({started: true});
+    try {
+      let watchID = Geolocation.watchPosition(
+        (info: any) => {
+          console.log('Tracking -> watchLocation -> info', info);
+          let {points} = this.state;
+          points.push({...info.coords, timestamp: info.timestamp});
+          console.table(points);
+          this.setState({
+            points,
+          });
+        },
+        (err: any) => {
+          console.log('TCL: Location -> watchLocation -> err', err);
+          RNAndroidLocationEnabler.promptForEnableLocationIfNeeded({
+            interval: 10000,
+            fastInterval: 5000,
+          })
+            .then((data: any) => {
+              this.watchLocation(false);
+            })
+            .catch((err: any) =>
+              this.showError('Veuillez activer la localisation et réessayer'),
+            );
+        },
+        {enableHighAccuracy: true},
+      );
       this.setState({watchID});
     } catch (error) {
       console.error('error getting location: ', error);
@@ -87,8 +134,17 @@ export default class Tracking extends Component<{
     }
   }
 
+  stopGettingLocation() {
+    Geolocation.clearWatch(this.state.watchID);
+    this.setState({started: false, finished: true});
+  }
+
   componentWillUnmount() {
     Geolocation.clearWatch(this.state.watchID);
+  }
+
+  onChange(location: any) {
+    console.log('Tracking -> onChange -> location', location.target());
   }
 
   render() {
@@ -100,12 +156,14 @@ export default class Tracking extends Component<{
           tapped={() => (this.props.navigation as any).toggleDrawer()}
           title="Tracking"
         />
-        {!this.state.started && (
-          <View
-            style={{flex: 1, justifyContent: 'center', paddingHorizontal: 50}}>
-            {this.state.error && (
-              <Text style={styles.error}>{this.state.message}</Text>
-            )}
+        <View
+          style={{
+            flex: 1,
+            paddingTop: 20,
+            paddingHorizontal: 50,
+            justifyContent: 'center',
+          }}>
+          {!this.state.started ? (
             <TouchableOpacity
               style={{
                 padding: 20,
@@ -114,20 +172,40 @@ export default class Tracking extends Component<{
               }}
               onPress={() => this.getLocation()}>
               <Text style={{textAlign: 'center', fontSize: 18, color: 'white'}}>
-                Demarrer un trajet
+                START
               </Text>
             </TouchableOpacity>
-          </View>
-        )}
-        <View>
-          {this.state.points.map((point: any, i: number) => (
-            <ListItem
-              key={i}
-              title={point.latitude + ',' + point.longitude}
-              bottomDivider
-              chevron
-            />
-          ))}
+          ) : (
+            <TouchableOpacity
+              style={{
+                padding: 20,
+                backgroundColor: 'red',
+                borderRadius: 20,
+              }}
+              onPress={() => this.stopGettingLocation()}>
+              <Text style={{textAlign: 'center', fontSize: 18, color: 'white'}}>
+                STOP
+              </Text>
+            </TouchableOpacity>
+          )}
+          {this.state.error && (
+            <Text style={styles.error}>{this.state.message}</Text>
+          )}
+        </View>
+        <View style={{flex: 5}}>
+          {this.state.location ? (
+            <MapView
+              initialRegion={{
+                latitude: this.state.location.latitude,
+                longitude: this.state.location?.longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              }}
+              showsUserLocation
+              style={{width: '100%', height: '100%'}}>
+              <Polygon coordinates={this.state.points} />
+            </MapView>
+          ) : null}
         </View>
       </View>
     );
--- components/Tracking/style.ts
+++ components/Tracking/style.ts
@@ -1,7 +1,7 @@
 import {StyleSheet, Dimensions} from 'react-native';
 
 const styles = StyleSheet.create({
-  error: {color: 'red', fontSize: 14},
+  error: {color: 'red', fontSize: 14, textAlign: 'center'},
 });
 
 export default styles;
