27c8141d1180228404992b4fa566a70d7ea5d761
MouadKH9 <mouad.khchich@gmail.com>
2020-04-18 23:24:49 +0100

    Getting the mac address

--- App.tsx
+++ App.tsx
@@ -2,9 +2,13 @@ import 'react-native-gesture-handler';
 import React, {Component} from 'react';
 import Navigator from './navigation/Navigator';
 import DatabaseManager from './managers/database/manager';
+import {getMacAddress} from 'react-native-device-info';
 export default class App extends Component {
-  componentDidMount() {
+  async componentDidMount() {
     DatabaseManager.prototype.connect();
+    // Getting and sending the mac address
+    const mac = await getMacAddress();
+    console.log('App -> componentDidMount -> mac', mac);
   }
   render() {
     return <Navigator />;
