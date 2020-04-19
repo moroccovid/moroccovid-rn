54f2d8147a635cd318deb57ed360d15910835f2b
MouadKH9 <mouad.khchich@gmail.com>
2020-04-19 19:10:20 +0000

    Progress on survey

--- components/Survey/Survey.tsx
+++ components/Survey/Survey.tsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Picker} from 'react-native';
 import styles from './style';
 import {
   NavigationScreenProp,
@@ -9,13 +9,12 @@ import {
 import Loading from '../utils/Loading/Loading';
 import Header from '../utils/Header/Header';
 import {ProgressSteps, ProgressStep} from 'react-native-progress-steps';
-import {Input} from 'react-native-elements';
-import Perso from './Perso';
-
+import {Input, CheckBox} from 'react-native-elements';
+import colors from '../../theme/colors';
 export default class Survey extends Component<{
   navigation: NavigationScreenProp<NavigationState, NavigationParams>;
 }> {
-  state: any = {loading: false};
+  state: any = {loading: false, sexe: 'homme'};
   componentDidMount() {}
   /**
    * First step: (age,sexe)
@@ -35,6 +34,8 @@ export default class Survey extends Component<{
   // --Toux sèche.
   // --Toux Avec mucus.
   // Depuis combien du temps avez-vous toussé ?
+
+  persoDone = () => {};
   render() {
     return this.state.loading ? (
       <Loading />
@@ -45,15 +46,68 @@ export default class Survey extends Component<{
           tapped={() => (this.props.navigation as any).toggleDrawer()}
         />
         <View style={{flex: 9}}>
-          <ProgressSteps>
-            <ProgressStep label="Information personel" nextBtnText="Suivant">
+          <ProgressSteps activeStep={0}>
+            <ProgressStep
+              nextBtnDisabled={
+                !this.state.age ||
+                !this.state.sexe ||
+                (this.state.chronique && !this.state.maladie)
+              }
+              label="Information personel"
+              nextBtnText="Suivant">
               <View style={styles.stepView}>
-                <Perso />
+                <Text style={styles.label}>Votre age:</Text>
+                <Input
+                  onChangeText={(age) => this.setState({age})}
+                  placeholder="Age"
+                  value={this.state.age}
+                  keyboardType="number-pad"
+                  containerStyle={{marginTop: 20}}
+                />
+
+                <Text style={styles.label}>Votre sexe:</Text>
+                <Picker
+                  style={{marginHorizontal: 20, marginTop: 20}}
+                  selectedValue="homme"
+                  onValueChange={(sexe) => this.setState({sexe})}>
+                  <Picker.Item label="Homme" value="homme" />
+                  <Picker.Item label="Femme" value="femme" />
+                </Picker>
+
+                <CheckBox
+                  containerStyle={{
+                    marginTop: 20,
+                    backgroundColor: 'transparent',
+                  }}
+                  checkedColor={colors.primary}
+                  onPress={() =>
+                    this.setState({chronique: !this.state.chronique})
+                  }
+                  textStyle={{fontSize: 16, fontWeight: 'normal'}}
+                  title="Avez-vous actuellement une maladie chronique?"
+                  checked={this.state.chronique}
+                />
+
+                {this.state.chronique && (
+                  <Input
+                    onChangeText={(maladie) => this.setState({maladie})}
+                    placeholder="Votre maladie"
+                    value={this.state.maladie}
+                    containerStyle={{marginTop: 20}}
+                  />
+                )}
               </View>
             </ProgressStep>
             <ProgressStep label="Second Step">
-              <View style={{alignItems: 'center'}}>
-                <Text>This is the content within step 2!</Text>
+              <View style={styles.stepView}>
+                <Text style={styles.label}>Votre sexe:</Text>
+                <Picker
+                  style={{marginHorizontal: 20, marginTop: 20}}
+                  selectedValue="homme"
+                  onValueChange={(sexe) => this.setState({sexe})}>
+                  <Picker.Item label="Homme" value="homme" />
+                  <Picker.Item label="Femme" value="femme" />
+                </Picker>
               </View>
             </ProgressStep>
           </ProgressSteps>
